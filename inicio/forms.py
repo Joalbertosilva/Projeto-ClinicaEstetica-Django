f4d6f06f7748ba425fe9a8bc89fc3d2208c50e10
unknown <joao.bbb189@gmail.com>
2025-06-02 14:02:28 -0300

    Criação do forms.py para otimizar o meu login e cadastro, com a presença de duas classes

--- inicio/forms.py
+++ inicio/forms.py
@@ -2,11 +2,15 @@ from django import forms
 from .models import Usuario
 
 class UsuarioFormulario(forms.ModelForm):
-    senha = forms.CharField(widget=forms.PasswordInput, label='Senha')  # Campo para senha
+    senha = forms.CharField(widget=forms.PasswordInput, label='Senha')  
 
     class Meta:
         model = Usuario
         fields = ['nome', 'email', 'endereco', 'telefone', 'senha']
         widgets = {
-            'endereco': forms.Textarea(attrs={'rows': 3, 'cols': 40}),  # Ajusta o tamanho do textarea
+            'endereco': forms.Textarea(attrs={'rows': 3, 'cols': 40}),  
         }
+
+class LoginFormulario(forms.Form):
+    email = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    senha = forms.CharField(label='Senha', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
